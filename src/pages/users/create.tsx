3bc1d2ccd0ced8b71b8c45a28cfb048bfc3983fe
Estevão Gabriel Biondi <teh.gabriel10@gmail.com>
2022-04-21 17:45:30 -0300

    Formularios e validação / Construindo interfaces do futuro

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,11 +1,42 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import Link from "next/link";
-import { Input } from "../../components/Form/Input";
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from 'yup'
 
+import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
+  password_confirmation: yup.string().oneOf([
+    null, yup.ref('password')
+  ], 'As senhas precisam ser iguais')
+})
+
 export default function CrateUser() {
+  const { register, handleSubmit, formState } = useForm<CreateUserFormData>({
+    resolver: yupResolver(createUserFormSchema)
+  })
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(values)
+  }
+
+  const { errors } = formState
+
   return (
     <Box>
       <Header />
@@ -17,8 +48,10 @@ export default function CrateUser() {
         <Sidebar />
 
         <Box
+          as="form"
           flex="1" borderRadius={8} bg="gray.800"
           p={["6", "8"]}
+          onSubmit={handleSubmit(handleCreateUser)}
         >
           <Heading
             size="lg" fontWeight="normal"
@@ -35,14 +68,16 @@ export default function CrateUser() {
               minWidth="240px" spacing={["6", "8"]} w="100%"
             >
               <Input
-                name="name"
                 type="text"
                 label="Nome completo"
+                error={errors.name}
+                { ...register("name") }
               />
               <Input
-                name="email"
                 type="email"
                 label="E-mail"
+                error={errors.email}
+                { ...register("email") }
               />
             </SimpleGrid>
 
@@ -50,14 +85,16 @@ export default function CrateUser() {
               minWidth="240px" spacing={["6", "8"]} w="100%"
             >
               <Input
-                name="password"
                 type="password"
                 label="Senha"
+                error={errors.password}
+                { ...register("password") }
               />
               <Input
-                name="password_confirmation"
                 type="password"
                 label="Confirmação da senha"
+                error={errors.password_confirmation}
+                { ...register("password_confirmation") }
               />
             </SimpleGrid>
           </VStack>
@@ -75,7 +112,9 @@ export default function CrateUser() {
                 </Button>
               </Link>
               <Button
+                type="submit"
                 colorScheme="pink"
+                isLoading={formState.isSubmitting}
               >
                 Salvar
               </Button>
